e48cee28398cacb9b2abc06e87b4f63a9e4c96b8
hacktiv8 <hacktiv8@H8JKT007.local>
2018-01-08 15:40:49 +0700

    Finished code

--- money_coins.js
+++ money_coins.js
@@ -1,3 +1,16 @@
-function money_coins (num) {
+function convertToCoin(num) {
   // your implementation code here
+  var result = 0;
+  var arr = [];
+  var money = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+  
+  for(var i = 0; i < money.length; i++) {
+    while(num%money[i] < num) {
+      arr.push(money[i]);
+      num -= money[i];
+    }
+  }
+  return arr;
 }
+
+convertToCoin(25524);
