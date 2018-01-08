8408ad223781b9e380e53a8ef280a4b941c8428b
hacktiv8 <hacktiv8@H8JKT007.local>
2018-01-08 15:45:32 +0700

    Added Pseudocode

--- money_coins.js
+++ money_coins.js
@@ -1,6 +1,20 @@
+//PSEUDOCODE
+/*
+Buat fungsi convertToCoin dengan num sebagai parameter
+	INITIALIZE var array kosong
+	INITIALIZE var money, yang berisi kelipatan uang yang telah ditetapkan
+
+	FOR var i=0 hingga money.length, i++
+		WHILE num%money[i] < num
+			PUSH money[i] ke arr
+			SUBSTRACT num dengan money[i]
+		END WHILE
+	END FOR
+END OF FUNCTION
+*/
+
 function convertToCoin(num) {
   // your implementation code here
-  var result = 0;
   var arr = [];
   var money = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
   
