0dbb77c8a11b07d4caa2fdee9c5864146ef44ed4
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-01-08 19:28:05 +0700

    Update

--- money_coins.js
+++ money_coins.js
@@ -1,3 +1,26 @@
-function money_coins (num) {
-  // your implementation code here
+function money_coins (uang) {
+  var kamusCoin = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+  var kamusUang = [10000,5000,2000,1000,500,200,100,50,20,10,1]
+
+  var coin = ""
+
+  for(var i = uang; i >= 0;i--){
+      for(var j = 0; j <= kamusUang.length - 1; j++){
+          if(uang >= kamusUang[j]){
+              coin+= kamusCoin[j];
+              uang -= kamusUang[j];
+              break;
+              j = 0
+          }
+      }
+
+      i = uang
+  }
+
+  return coin
 }
+
+console.log(money_coins(1))
+console.log(money_coins(10))
+console.log(money_coins(170))
+console.log(money_coins(3500))
