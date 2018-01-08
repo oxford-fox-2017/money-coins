f2bd9bdf7896e74aebf11c2e775c22ea6028e4d5
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-01-08 15:40:41 +0700

    Finish convert coin from large to small

--- money_coins.js
+++ money_coins.js
@@ -1,3 +1,14 @@
 function money_coins (num) {
   // your implementation code here
+  let counter = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+  let pecahan = [];
+  for ( let i = counter.length-1; i >= 0; i--){
+    if( num >= counter[i]){
+      pecahan.push(counter[i]);
+      num-= counter[i];
+    }
+  }
+  return pecahan;
 }
+
+console.log(money_coins(6500));
