d05ca0a085b770909ecbfba1cd84408a873b348a
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-01-08 19:13:29 +0700

    Money Coins Release

--- money_coins.js
+++ money_coins.js
@@ -1,3 +1,15 @@
-function money_coins (num) {
-  // your implementation code here
+function convertToCoin(num) {
+    let tampung = [];
+    let coin = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+
+    for (let i = 0; i < coin.length - 1; i++) {
+        while (num >= coin[i]) {
+            tampung.push(coin[i]);
+            num -= coin[i];
+        }
+    }
+    return tampung;
 }
+
+console.log(convertToCoin(15000));
+console.log(convertToCoin(25500));
