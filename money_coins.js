811f38f451d20bc8358ab34e32704fe0db43e959
aldiws <syldie.aldiwijaya@gmail.com>
2018-01-08 17:39:16 +0700

    money-coins-done

--- money_coins.js
+++ money_coins.js
@@ -1,3 +1,14 @@
-function money_coins (num) {
+function money_coins(num) {
   // your implementation code here
+  let pecahan = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
+  let result = []
+  for (let i = 0; i < pecahan.length; i++) {
+    while (num >= pecahan[i]) {
+      result.push(pecahan[i])
+      num -= pecahan[i]
+    }
+  }
+  return result
 }
+
+console.log(money_coins(25000))
